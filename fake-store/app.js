9d90ac8cfebc01e0f86de48e21a57e5098a28d11
Seif Sheikhelarab <seifusama@gmail.com>
2025-02-19 18:40:31 +0200

    Update app.js

--- fake-store/app.js
+++ fake-store/app.js
@@ -3,6 +3,6 @@ fetch('https://fakestoreapi.com/products/')
             .then(products=>{
                 let sum = products[0].price * 3
                     + products[3].price * 4
-                    + products[5].price * 5
+                    + products[2].price * 5
                 console.log(`the total price is ${sum}$`);
             })
